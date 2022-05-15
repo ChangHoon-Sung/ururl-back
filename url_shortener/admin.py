e1f39cb16030ff1dfee40b2eddb553a17fc1d0fa
ChangHoon Sung <changhoon.dev@gmail.com>
2022-05-16 00:44:25 +0900

    Add custom model on admin site

--- url_shortener/admin.py
+++ url_shortener/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import RandomURL
+from .models import RandomURL, CustomURL
+
 admin.site.register(RandomURL)
+admin.site.register(CustomURL)
